e22e23e972a2eb7186f515d2aa92c3c31c3753fb
masc1005 <018210007@unifacs.edu.br>
2022-01-21 18:02:07 -0300

    :sparkles: criptografia de senha

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,15 +1,18 @@
 import { Request, Response } from 'express';
+import bcryptjs from 'bcryptjs'
 import { prismaClient } from '../database/prismaClient';
 
 class User {
   async create(req: Request, res: Response) {
-    const { name, email, password } = req.body
+    const { name, email, pwd } = req.body
 
     const userExists = await prismaClient.user.findFirst({ where: {email: email} })
 
     if(userExists){
       return res.sendStatus(409)
     }
+
+    const password = await bcryptjs.hash(pwd, 10)    
 
     const user = await prismaClient.user.create({
       data: {
