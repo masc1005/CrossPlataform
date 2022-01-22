c1fa79e653e5926369f4fc3ef038d69a09f8c5ff
Eduardo <eduardosram10@gmail.com>
2022-01-22 18:31:43 -0300

    Criação da função Read e Delete na Role

--- src/controllers/roleController.ts
+++ src/controllers/roleController.ts
@@ -5,9 +5,9 @@ class Role {
   async create(req: Request, res: Response) {
     const { name, description } = req.body;
 
-    const roleExits = await prismaClient.role.findFirst({ where: {name : name} })
+    const roleExits = await prismaClient.role.findFirst({ where: { name: name } })
 
-    if (roleExits){
+    if (roleExits) {
       return res.sendStatus(409)
     }
 
@@ -18,5 +18,30 @@ class Role {
     })
     return res.json(role);
   }
+
+  async read(req: Request, res: Response) {
+
+    const roles = await prismaClient.role.findMany()
+
+    return res.json(roles);
+
+  }
+
+  async delete(req: Request, res: Response) {
+
+    const { name } = req.body;
+
+    const roleExits = await prismaClient.role.findFirst({ where: { name: name } })
+
+    if (roleExits) {
+      return res.sendStatus(404)
+    }
+
+    await prismaClient.role.delete({ where: { name: name } })
+
+    return res.send("Deletado com sucesso");
+  }
+
+
 }
 export default new Role();
